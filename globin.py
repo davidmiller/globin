c522399a6cd61299499b66dd9f541348c117a5e8
David Miller <il.livid.dream@gmail.com>
2009-10-16 22:18:20 +0100

    Actually works! & more abstracted.

--- globin.py
+++ globin.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
-""" command line utility for adding scripts to local & global $PATH"""
+"""57 command line utility for adding scripts to local & global $PATH"""
 import os
 import sys
 import argparse
 
+# Error classes to hanle possible overwriting conflicts
 class Error( Exception ):
     """ Base class for exceptions """
     pass
+
 
 class ExistsInPathError( Error ):
     """ Don't go creating the created now"""
@@ -16,14 +18,20 @@ class ExistsInPathError( Error ):
         return repr( self.value )
 
 
+def link_if_empty( target, destination ):
+    if not os.path.isfile( destination ):
+        os.symlink( script, global_link_location )
+    else:
+        raise ExistsInPathError( global_link_location )
 
 if __name__ == '__main__':
+    # Argument parsing stuff
     program = 'globin'
     parser = argparse.ArgumentParser( prog = program )    
     parser.add_argument( 'program',
                          help = 'name of the program - also the command you will use from stdin')
     parser.add_argument( 'path',
-                         help = "the path to the program you want to add to the global path"
+                         help = "the path to the program you want to add to the global path" )
     args = parser.parse_args()    
 
     # Get the script location
@@ -31,24 +39,20 @@ if __name__ == '__main__':
     if not os.path.isfile( script ):
         print 'the path to the script you entered turns out not to be a file'
         sys.exit()
+    
+    # Link location definitions
+    global_link_location = os.path.join( '/usr/local/bin', args.program )
+    home_link_loc = os.path.join( home_bin_path, program )
 
     # Put the script into the global path
     if os.access( '/usr/local/bin', os.W_OK ):
-        global_link_location = os.path.join( '/usr/local/bin', args.program )
-        if not os.path.isfile( global_link_location ):
-            os.symlink( script, global_link_location )
-        else:
-            raise ExistsInPathError( global_link_location )
+        link_if_empty( script, global_link_location )
     else:
         print """ No write access to global path - file only added locally"""         
 
     # Put the script into the local path
-    home_bin_path = os.path.join(  os.environ['HOME'] 'bin' )
+    home_bin_path = os.path.join(  os.environ['HOME'], 'bin' )    
     if os.path.isdir( home_bin_path ):
-        home_link_loc = os.path.join( home_bin_path, program )
-        if not os.path.isfile( home_link_loc ):
-            os.symlink( script, home_link_location )                
-        else:
-            raise ExistsInPathError( home_link_location )
+        link_if_empty( script, home_link_loc )
     
     print "%s added to $PATH" % program
