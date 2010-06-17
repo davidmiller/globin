6f80717a002af87a9526af9914116240ef3c58d8
David <david@deadpansincerity.com>
2010-06-17 16:12:03 +0100

    restructure && setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+from distutils.core import setup, Extension
+
+setup(name="Globin",
+      version="0.1",
+      description="Puts a script into path",
+      author="David Miller",
+      author_email="david@deadpansincerity.com",
+      url="http://www.deadpansincerity.com",
+      packages=['globin']
+      )
